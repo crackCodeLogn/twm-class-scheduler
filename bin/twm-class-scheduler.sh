ea30639f1efc794db724c2a4c25f4dc4a1539a79
Vivek <vivektheintel@gmail.com>
2022-07-22 22:34:42 +0530

    Updating bin script file to feed path of data file

--- bin/twm-class-scheduler.sh
+++ bin/twm-class-scheduler.sh
@@ -5,5 +5,7 @@ JAVA_PARAM="-Xmx251m"
 BIN_PATH=$TWM_HOME_PARENT/TWM/$APP_NAME/bin     #TWM-HOME-PARENT :: exported in .bashrc
 JAR_PATH=$BIN_PATH/../target/$APP_NAME-$APP_VERSION.jar
 
-echo "Starting '$APP_NAME' with java param: '$JAVA_PARAM', at '$JAR_PATH'"
-java $JAVA_PARAM -jar $JAR_PATH
+APP_PARAMS="-Dscheduler.location=${BIN_PATH}/../landing/class-schedules.active.json"
+
+echo "Starting '$APP_NAME' with java param: '$JAVA_PARAM', app params: '$APP_PARAMS' at '$JAR_PATH'"
+java $JAVA_PARAM $APP_PARAMS -jar $JAR_PATH
